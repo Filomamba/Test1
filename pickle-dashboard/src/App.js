464cc9a1147a5ccff66ede223558422e7492ee26
Filomamba <u7104300@anu.edu.au>
2024-11-11 23:40:54 +1100

    Update App.js

--- pickle-dashboard/src/App.js
+++ pickle-dashboard/src/App.js
@@ -1,25 +1,29 @@
 // src/App.js
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navigation from './components/Navigation';
-import Home from './components/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import MenuManager from './components/MenuManager';
 import Analytics from './components/Analytics';
 import Access from './components/Access';
 import Support from './components/Support';
+import Navbar from './components/Navbar';
 
-export default function App() {
+function App() {
   return (
     <Router>
       <div>
-        <Navigation />
+        <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Dashboard />} />
           <Route path="/menu-manager" element={<MenuManager />} />
           <Route path="/analytics" element={<Analytics />} />
           <Route path="/access" element={<Access />} />
           <Route path="/support" element={<Support />} />
         </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
